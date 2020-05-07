dc4c82d906ef7710983068f65df917c03a041b93
Ivana Pantazi <IvanaPantazi@redcross.org.uk>
2020-05-07 12:24:53 +0100

    Updates

---
+++ precache-manifest.d57de3adc78d0115e2f207ad0d0d4856.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca8910957ad44e9d40b4271bd519dc2",
+    "url": "/ivpweb/index.html"
+  },
+  {
+    "revision": "f758375fcf65fe5c4ac0",
+    "url": "/ivpweb/static/css/2.cb21d0f4.chunk.css"
+  },
+  {
+    "revision": "52220edbd67713284ecd",
+    "url": "/ivpweb/static/css/main.a335ebfe.chunk.css"
+  },
+  {
+    "revision": "f758375fcf65fe5c4ac0",
+    "url": "/ivpweb/static/js/2.22c20f2e.chunk.js"
+  },
+  {
+    "revision": "e99c6a16d2c2ccc3e4d601f24d94e183",
+    "url": "/ivpweb/static/js/2.22c20f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52220edbd67713284ecd",
+    "url": "/ivpweb/static/js/main.90976e5b.chunk.js"
+  },
+  {
+    "revision": "2cfff2576789c91dd5c7",
+    "url": "/ivpweb/static/js/runtime-main.927a8e7a.js"
+  },
+  {
+    "revision": "152808faa461d9b34f69ea2b4913b82c",
+    "url": "/ivpweb/static/media/boosey.152808fa.png"
+  },
+  {
+    "revision": "e5e8ad42fe00ba6cb382f6d6be0f3a8e",
+    "url": "/ivpweb/static/media/cf_source.e5e8ad42.png"
+  },
+  {
+    "revision": "9e50acb53081ac56cd9faef9974d170d",
+    "url": "/ivpweb/static/media/first_aid_champions.9e50acb5.png"
+  },
+  {
+    "revision": "f9bb38bdac0eab9f8d9d3570d757601c",
+    "url": "/ivpweb/static/media/flu_facts.f9bb38bd.png"
+  },
+  {
+    "revision": "a6ea2a5a93205efec37968a178d1a3a4",
+    "url": "/ivpweb/static/media/ipo.a6ea2a5a.png"
+  },
+  {
+    "revision": "3179708ecbee6ec95c39e90b5ab83ecd",
+    "url": "/ivpweb/static/media/lcm.3179708e.png"
+  },
+  {
+    "revision": "4fe6f9caff8b287170d51d3d71d5e5c6",
+    "url": "/ivpweb/static/media/lg.4fe6f9ca.ttf"
+  },
+  {
+    "revision": "5fd4c338c1a1b1eeeb2c7b0a0967773d",
+    "url": "/ivpweb/static/media/lg.5fd4c338.woff"
+  },
+  {
+    "revision": "c066c5448562b3ccaefb6408ce4b4ae1",
+    "url": "/ivpweb/static/media/lg.c066c544.svg"
+  },
+  {
+    "revision": "ecff11700aad0000cf3503f537d1df17",
+    "url": "/ivpweb/static/media/lg.ecff1170.eot"
+  },
+  {
+    "revision": "6d231c5292f6bc64b334fa06d4a3c5d3",
+    "url": "/ivpweb/static/media/logo.6d231c52.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ivpweb/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ivpweb/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ivpweb/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ivpweb/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "738ca0f798e5acbccad1c64b6aeecccb",
+    "url": "/ivpweb/static/media/somerset_house.738ca0f7.png"
+  },
+  {
+    "revision": "c274810f47d42fe024ab9bf15f52c06e",
+    "url": "/ivpweb/static/media/stories.c274810f.png"
+  },
+  {
+    "revision": "b899189b404a716fc2f8e89b3f5df567",
+    "url": "/ivpweb/static/media/team-1.b899189b.jpg"
+  },
+  {
+    "revision": "cc594e8ccd25d245c4641034729284e1",
+    "url": "/ivpweb/static/media/team-2.cc594e8c.jpg"
+  },
+  {
+    "revision": "362c28fc84ba079d4914a90e6a133e88",
+    "url": "/ivpweb/static/media/team-3.362c28fc.jpg"
+  },
+  {
+    "revision": "c56eaf3e999b75bce6db4791387f7962",
+    "url": "/ivpweb/static/media/vertex.c56eaf3e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eefa4c993592d53632d65b0f8cd451c2.js"
+  "/ivpweb/precache-manifest.d57de3adc78d0115e2f207ad0d0d4856.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ivpweb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
