76603677a8fa0d207553879bd1abaf44a2123054
Ivana Pantazi <IvanaPantazi@redcross.org.uk>
2020-05-07 12:14:56 +0100

    Updates

---
+++ precache-manifest.eefa4c993592d53632d65b0f8cd451c2.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1350c24bc5b6ba8db402a4bec568b29c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3998a8d9638f976196c5",
+    "url": "/static/css/2.4c6ac84f.chunk.css"
+  },
+  {
+    "revision": "0d9c7b1830163bcc33d6",
+    "url": "/static/css/main.a335ebfe.chunk.css"
+  },
+  {
+    "revision": "3998a8d9638f976196c5",
+    "url": "/static/js/2.04fe7032.chunk.js"
+  },
+  {
+    "revision": "e99c6a16d2c2ccc3e4d601f24d94e183",
+    "url": "/static/js/2.04fe7032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d9c7b1830163bcc33d6",
+    "url": "/static/js/main.06dc1867.chunk.js"
+  },
+  {
+    "revision": "00b56ab67724e782a96e",
+    "url": "/static/js/runtime-main.0abb9038.js"
+  },
+  {
+    "revision": "152808faa461d9b34f69ea2b4913b82c",
+    "url": "/static/media/boosey.152808fa.png"
+  },
+  {
+    "revision": "e5e8ad42fe00ba6cb382f6d6be0f3a8e",
+    "url": "/static/media/cf_source.e5e8ad42.png"
+  },
+  {
+    "revision": "9e50acb53081ac56cd9faef9974d170d",
+    "url": "/static/media/first_aid_champions.9e50acb5.png"
+  },
+  {
+    "revision": "f9bb38bdac0eab9f8d9d3570d757601c",
+    "url": "/static/media/flu_facts.f9bb38bd.png"
+  },
+  {
+    "revision": "a6ea2a5a93205efec37968a178d1a3a4",
+    "url": "/static/media/ipo.a6ea2a5a.png"
+  },
+  {
+    "revision": "3179708ecbee6ec95c39e90b5ab83ecd",
+    "url": "/static/media/lcm.3179708e.png"
+  },
+  {
+    "revision": "4fe6f9caff8b287170d51d3d71d5e5c6",
+    "url": "/static/media/lg.4fe6f9ca.ttf"
+  },
+  {
+    "revision": "5fd4c338c1a1b1eeeb2c7b0a0967773d",
+    "url": "/static/media/lg.5fd4c338.woff"
+  },
+  {
+    "revision": "c066c5448562b3ccaefb6408ce4b4ae1",
+    "url": "/static/media/lg.c066c544.svg"
+  },
+  {
+    "revision": "ecff11700aad0000cf3503f537d1df17",
+    "url": "/static/media/lg.ecff1170.eot"
+  },
+  {
+    "revision": "6d231c5292f6bc64b334fa06d4a3c5d3",
+    "url": "/static/media/logo.6d231c52.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "738ca0f798e5acbccad1c64b6aeecccb",
+    "url": "/static/media/somerset_house.738ca0f7.png"
+  },
+  {
+    "revision": "c274810f47d42fe024ab9bf15f52c06e",
+    "url": "/static/media/stories.c274810f.png"
+  },
+  {
+    "revision": "b899189b404a716fc2f8e89b3f5df567",
+    "url": "/static/media/team-1.b899189b.jpg"
+  },
+  {
+    "revision": "cc594e8ccd25d245c4641034729284e1",
+    "url": "/static/media/team-2.cc594e8c.jpg"
+  },
+  {
+    "revision": "362c28fc84ba079d4914a90e6a133e88",
+    "url": "/static/media/team-3.362c28fc.jpg"
+  },
+  {
+    "revision": "c56eaf3e999b75bce6db4791387f7962",
+    "url": "/static/media/vertex.c56eaf3e.png"
+  }
+]);
